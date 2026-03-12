04a671f98af8a18152a0063c4900b2e6cad04fa4
Jud1714 <juandiegolopezarias07@gmail.com>
2026-03-12 10:42:01 -0500

    feat: add default color adjustments constants for video and image processing

---
+++ src/constants/colorAdjustments.ts
@@ -0,0 +1,11 @@
+import type { ColorAdjustments } from "../project/projectTypes"
+
+export const DEFAULT_COLOR_ADJUSTMENTS: ColorAdjustments = {
+  brightness: 0,
+  contrast: 0,
+  saturation: 1,
+  gamma: 1,
+  exposure: 0,
+  shadow: 0,
+  definition: 0,
+}
